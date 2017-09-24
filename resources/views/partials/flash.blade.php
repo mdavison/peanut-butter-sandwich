9991478643c5839d840c112d1c2c1e04dc0e074a
Morgan Davison <morgan.davison@gmail.com>
2017-09-24 15:32:31 -0700

    Oops, I needed that view after all; Tweak the flash messages; Change redirectTo url when resetting password

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -4,6 +4,18 @@
     </div>
 @endif
 
+@if (session('status') && !session('errors') && !session('success'))
+    <div class="alert alert-info" role="alert">
+        {{ session('status') }}
+    </div>
+@endif
+
+@if (session('success'))
+    <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+    </div>
+@endif
+
 @if (session('error'))
     <div class="alert alert-danger" role="alert">
         {{ session('error') }}
