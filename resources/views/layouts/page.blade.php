0df05dc8313d58552537108a1e29f82c05574e13
Morgan Davison <morgan.davison@gmail.com>
2017-10-16 05:43:53 -0700

    Improve font on typing game page

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Delius" rel="stylesheet">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
